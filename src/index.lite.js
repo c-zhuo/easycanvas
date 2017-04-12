46537e9c6e598bfef540ccd9b3e89d707cd51f24
chenzhuo1992 <13654545071@qq.com>
2017-04-12 19:11:04 +0800

    lite

---
+++ src/index.lite.js
@@ -0,0 +1,13 @@
+import painter from './utils/painter.js';
+import tick from './utils/tick.js';
+import mirror from './utils/mirror.js';
+import imgLoader from './utils/img-loader.js';
+import posCompare from './utils/position-compare.js';
+
+module.exports = {
+    painter,
+    imgLoader,
+    posCompare,
+    tick,
+    mirror,
+};
