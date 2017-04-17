00f19e39417f3acd5aa465e68934669c0e3ceb20
chenzhuo1992 <13654545071@qq.com>
2017-04-17 20:23:31 +0800

    bugfix &:wq  preAdd

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import tick from './utils/tick.js';
 import mirror from './utils/mirror.js';
 import imgLoader from './utils/img-loader.js';
 import random from './utils/random.js';
+import transition from './utils/transition.js';
 import gif2canvas from './utils/gif2canvas.js';
 import posCompare from './utils/position-compare.js';
 
@@ -12,6 +13,7 @@ module.exports = {
     gif2canvas,
     posCompare,
     random,
+    transition,
     tick,
     mirror,
 };
