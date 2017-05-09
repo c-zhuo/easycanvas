f9a809445cea145ad9d3ac0e6c0159ee0a085db6
chenzhuo1992 <13654545071@qq.com>
2017-05-09 13:52:46 +0800

    merge

--- src/index.lite.js
+++ src/index.lite.js
@@ -2,6 +2,7 @@ import painter from './utils/painter.js';
 import tick from './utils/tick.js';
 import mirror from './utils/mirror.js';
 import utils from './utils/utils.js';
+import transition from './utils/transition.js';
 import imgLoader from './utils/img-loader.js';
 import posCompare from './utils/position-compare.js';
 
@@ -9,6 +10,7 @@ module.exports = {
     painter,
     imgLoader,
     posCompare,
+    transition,
     tick,
     utils,
     mirror,
