79cf3b321104dfd5b3d5bcc6c93c6c1978d563b0
陈卓 <chenzhuo04@meituan.com>
2017-05-09 13:43:31 +0800

    1

--- src/index.lite.js
+++ src/index.lite.js
@@ -1,15 +1,15 @@
 import painter from './utils/painter.js';
 import tick from './utils/tick.js';
 import mirror from './utils/mirror.js';
+import utils from './utils/utils.js';
 import imgLoader from './utils/img-loader.js';
-import transition from './utils/transition.js';
 import posCompare from './utils/position-compare.js';
 
 module.exports = {
     painter,
     imgLoader,
     posCompare,
-    transition,
     tick,
+    utils,
     mirror,
 };
--- src/utils/img-loader.js
+++ src/utils/img-loader.js
@@ -1,24 +1,54 @@
 var Cache = {};
 
-module.exports = function (url, callback) {
-	if (Cache[url] && Cache[url] !== 'processing') {
+module.exports = function (url, callback, option) {
+	var cacheNamespace = url;
+	if (option) cacheNamespace += JSON.stringify(option);
+
+	if (Cache[cacheNamespace] && Cache[cacheNamespace] !== 'processing') {
 		setTimeout(function () {
 			if (callback) {
-				callback(Cache[url]);
+				callback(Cache[cacheNamespace]);
 			}
 		});
-		return Cache[url];
+		return Cache[cacheNamespace];
 	}
 
 	var i = new Image();
 	i.src = url;
-	Cache[url] = i;		
+	Cache[cacheNamespace] = 'processing';		
 
 	i.onload = function () {
+		var _option = option || {};
+		if (_option.alphaColor) {
+			var tempCavas = document.createElement('canvas');
+			var cts = tempCavas.getContext('2d');
+			tempCavas.width = i.width;
+			tempCavas.height = i.height;
+			cts.drawImage(i, 0, 0);
+
+			var data = cts.getImageData(0, 0, i.width, i.height);
+			var pixel = [];
+			for (var d = 0; d < data.data.length; d += 4) {
+				var perPixel = data.data.slice(d, d + 4);
+				var colorWeight = data.data[d] + data.data[d + 1] + data.data[d + 2];
+				var blackLike = 1;
+				if (data.data[d] < blackLike && data.data[d + 1] < blackLike && data.data[d + 2] < blackLike) {
+					data.data[d + 3] = parseInt(colorWeight / 255);
+				}
+			}
+			cts.putImageData(data, 0, 0);
+			i = tempCavas;
+		}
+		Cache[cacheNamespace] = i;
+
 		if (callback) {
 			callback(i);
 		}
-	}
+	};
+
+	i.onerror = function () {
+		Cache[cacheNamespace] = i;
+	};
 
 	return i;
 };
