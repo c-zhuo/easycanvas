c710f7a3367cf19d0f94464a611b1b4cfd5f4151
chenzhuo1992 <13654545071@qq.com>
2017-05-21 20:00:26 +0800

    update

--- src/utils/img-loader.js
+++ src/utils/img-loader.js
@@ -28,8 +28,8 @@ module.exports = function (url, callback, option) {
 
 			var data = cts.getImageData(0, 0, i.width, i.height);
 			var pixel = [];
+
 			for (var d = 0; d < data.data.length; d += 4) {
-				var perPixel = data.data.slice(d, d + 4);
 				var colorWeight = data.data[d] + data.data[d + 1] + data.data[d + 2];
 				var blackLike = 1;
 				if (data.data[d] < blackLike && data.data[d + 1] < blackLike && data.data[d + 2] < blackLike) {
--- src/utils/position-compare.js
+++ src/utils/position-compare.js
@@ -60,5 +60,9 @@ module.exports = {
 
     getDistanceSq: function (x1, y1, x2, y2) {
         return (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
-    }
+    },
+
+    enoughNear: function (x1, y1, x2, y2, r) {
+        return (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2) <= r * r;
+    },
 };
