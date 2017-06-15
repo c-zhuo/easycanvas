651a79caef65fa16a84a65b83e3841934f28c1a8
chenzhuo1992 <13654545071@qq.com>
2017-06-15 14:34:56 +0800

    support percent values

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,21 +1,27 @@
 module.exports = {
-	funcOrValue: function (funcOrValue, _this) {
-		if (typeof funcOrValue === 'function') {
-			return funcOrValue.call(_this);
-		}
+    funcOrValue: function (funcOrValue, _this) {
+        if (typeof funcOrValue === 'function') {
+            return funcOrValue.call(_this);
+        }
 
-		return funcOrValue;
-	},
+        return funcOrValue;
+    },
 
-	getMinFromArray: function (arr) {
-		var res = arr[0];
-		arr.forEach(function (item) {
-			if (item < res) res = item;
-		});
-		return res;
-	},
+    cent2value: function (cent, full) {
+        if (typeof cent !== 'string' || !cent || !full) return cent;
+        if (cent[cent.length - 1] === '%') return full * Number(cent.substr(0, cent.length - 1)) / 100;
+        return cent;
+    },
 
-	getCharFromKey: function (k) {
-		return k.key || String.fromCharCode(k.keyCode);
-	},
+    getMinFromArray: function (arr) {
+        var res = arr[0];
+        arr.forEach(function (item) {
+            if (item < res) res = item;
+        });
+        return res;
+    },
+
+    getCharFromKey: function (k) {
+        return k.key || String.fromCharCode(k.keyCode);
+    },
 };
