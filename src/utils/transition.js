7db006d0ae5e037088c708745cf0403d2f9ae3c4
chenzhuo1992 <13654545071@qq.com>
2017-06-16 09:57:23 +0800

    support 1by1 trasition

--- src/utils/transition.js
+++ src/utils/transition.js
@@ -1,31 +1,63 @@
+import utils from './utils.js';
+
 var transitions = [];
 
+var PI = 3.141593;
+
 var second2frame = function (second) {
-	return second / 1000 * 60;
+    return second / 1000 * 60;
 };
 
-module.exports = {
-	linear: function (a, b, duration) {
-		var l = transitions.length;
-		transitions.push(a);
-		return function () {
-			var current = transitions[l];
-			if ((current >= a && current < b) || (current > b && current <= a)) {
-				transitions[l] += (b - a) / second2frame(duration);
-			} else {
-				return b;
-			}
-			return current;
-		};
-	},
+var transFuncs = {
+    linear: function (a, b, duration) {
+        var l = transitions.length;
+        transitions.push(a);
+        var resFunc = function () {
+            var current = transitions[l];
+            if ((current >= a && current < b) || (current > b && current <= a)) {
+                transitions[l] += (b - a) / second2frame(duration);
+            } else {
+                return {
+                    $$value: b,
+                    $$over: true,
+                };
+            }
+            return {
+                $$value: current,
+                $$over: false,
+            };
+        };
+        return resFunc;
+    },
 
-	pendulum: function (a, b, duration) {
-		var l = transitions.length;
-		transitions.push(0);
-		return function () {
-			var current = Math.sin(transitions[l]) + 1; // 0 ~ 2
-			transitions[l] += 3.1416 / second2frame(duration);
-			return a + (b - a) * current / 2;
-		};
-	}
+    pendulum: function (a, b, duration) {
+        var l = transitions.length;
+        transitions.push(-PI);
+        var resFunc = function () {
+            var current = Math.cos(transitions[l]) + 1; // 0 ~ 2
+            transitions[l] += PI / second2frame(duration);
+            return {
+                $$value: utils.funcOrValue(a) + (utils.funcOrValue(b) - utils.funcOrValue(a)) * current / 2,
+                $$over: transitions[l] >= 3 * PI,
+            };
+        };
+        return resFunc;
+    },
+
+    oneByOne: function (arr) {
+        return function () {
+            for (var i = 0; i < arr.length; i++) {
+                var res = arr[i]();
+                if (typeof res === 'object') {
+                    if (!res.$$over) {
+                        return res.$$value;
+                    }
+                } else if (res !== false) {
+                    return res;
+                }
+            }
+        };
+    },
 };
+
+module.exports = transFuncs;
