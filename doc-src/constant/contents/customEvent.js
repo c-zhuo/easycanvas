40750b9fb7fc2b62c504007ad6e42c52704721fc
chenzhuo1992 <13654545071@qq.com>
2018-05-01 15:25:51 +0800

    docs update & bugfix

---
+++ doc-src/constant/contents/customEvent.js
@@ -0,0 +1,147 @@
+module.exports = `
+    <article id="自定义事件">
+        <h1>自定义事件</h1>
+
+        <p>自定义事件一般用于处理多个sprite之间的互相影响。</p>
+        
+        <p>可以通过on和off来为每个sprite对象绑定和解除自定义事件，然后通过trigger和broadcast触发。其中<strong>broadcast触发的事件广播不仅会在自身触发，还会传递给它的所有children；trigger只会在自身触发，不会向下传递</strong>。例如下例：</p>
+
+        <section>
+            <div class="code-2-demo bg-demo"></div>
+            <code>
+                <body>
+                    <canvas id="app"></canvas>
+                    <br>
+                    <button id="button1">trigger on parent</button>
+                    <button id="button2">broadcast on parent</button>
+                </body>
+
+                <script>
+                    var G = 'https://github.com/chenzhuo1992/easycanvas/blob/master/demos/G.png?raw=true';
+
+                    var $app = new Easycanvas.painter({
+                        el: '#app',
+                        width: 400,
+                        height: 400
+                    });
+
+                    var $parent = new Easycanvas.class.sprite({
+                        content: {
+                            img: G,
+                        },
+                        style: {
+                            tw: 100, th: 100, tx: 100, ty: 100, rotate: 0,
+                        },
+                    });
+
+                    var $child1 = $parent.add({
+                        content: {
+                            img: G,
+                        },
+                        style: {
+                            tw: 50, th: 50, tx: 50, ty: 100, rotate: 0,
+                        },
+                    });
+                    var $child2 = $parent.add({
+                        content: {
+                            img: G,
+                        },
+                        style: {
+                            tw: 50, th: 50, tx: 125, ty: 100, rotate: 0,
+                        },
+                    });
+
+                    var add90Deg = function () {
+                        var currentRotate = this.self().rotate;
+                        this.style.rotate = Easycanvas.transition.linear(currentRotate, currentRotate + 90, 500);
+                    };
+
+                    $parent.on('add90Deg', add90Deg);
+                    $child1.on('add90Deg', add90Deg);
+                    $child2.on('add90Deg', add90Deg, 2000);
+
+                    document.getElementById('button1').addEventListener('click', function () {
+                        $parent.trigger('add90Deg');
+                    });
+                    document.getElementById('button2').addEventListener('click', function () {
+                        $parent.broadcast('add90Deg');
+                    });
+
+                    $app.add($parent);
+                    $app.start();
+                </script>
+            </code>
+        </section>
+
+        <p>这上面例子中，如果频繁点击第二个按钮（在parent广播事件），可以看到child1会连续旋转，而child2不会。即使我们给parent也加上3秒的debounce，频繁触发broadcast的时候，child1仍然可以连续旋转。这是因为<strong>各个sprite对同一个事件的debounce是独立的</strong>。</p>
+
+        <p>自定义事件可以传递一些参数，例如下例：</p>
+
+        <section>
+            <div class="code-2-demo bg-demo"></div>
+            <code>
+                <body>
+                    <canvas id="app"></canvas>
+                    <br>
+                    <button id="button1">trigger 90deg on parent</button>
+                    <button id="button2">broadcast 45deg on parent</button>
+                </body>
+
+                <script>
+                    var G = 'https://github.com/chenzhuo1992/easycanvas/blob/master/demos/G.png?raw=true';
+
+                    var $app = new Easycanvas.painter({
+                        el: '#app',
+                        width: 400,
+                        height: 400
+                    });
+
+                    var $parent = new Easycanvas.class.sprite({
+                        content: {
+                            img: G,
+                        },
+                        style: {
+                            tw: 100, th: 100, tx: 100, ty: 100, rotate: 0,
+                        },
+                    });
+
+                    var $child1 = $parent.add({
+                        content: {
+                            img: G,
+                        },
+                        style: {
+                            tw: 50, th: 50, tx: 50, ty: 100, rotate: 0,
+                        },
+                    });
+                    var $child2 = $parent.add({
+                        content: {
+                            img: G,
+                        },
+                        style: {
+                            tw: 50, th: 50, tx: 125, ty: 100, rotate: 0,
+                        },
+                    });
+
+                    var addDeg = function (deg) {
+                        var currentRotate = this.self().rotate;
+                        this.style.rotate = Easycanvas.transition.linear(currentRotate, currentRotate + deg, 500);
+                    };
+
+                    $parent.on('addDeg', addDeg);
+                    $child1.on('addDeg', addDeg);
+                    $child2.on('addDeg', addDeg, 2000);
+
+                    document.getElementById('button1').addEventListener('click', function () {
+                        $parent.trigger('addDeg', 90);
+                    });
+                    document.getElementById('button2').addEventListener('click', function () {
+                        $parent.broadcast('addDeg', 45);
+                    });
+
+                    $app.add($parent);
+                    $app.start();
+                </script>
+            </code>
+        </section>
+    </article>
+`;
--- doc-src/constant/contents/image.js
+++ doc-src/constant/contents/image.js
@@ -4,6 +4,8 @@ module.exports = `
 
         <p>这里将介绍图片的裁剪、加载以及图片的像素预处理。</p>
         
+        <p>在“快速开始”的例子中，我们在content中增加了一个img属性，表示要渲染一张图片。当然，content也可以为空，代表这是一个容器。</p>
+
         <h2>图片裁剪</h2>
 
         <p>如果需要将图片源的某一个局部进行渲染，可以通过修改sprite的style样式中的sx、sy、sw、sh来控制。其中sx和sy是裁剪的起点，sw和sh是裁剪的尺寸。例如下图，将字母G的一部分进行了渲染。</p>
@@ -43,6 +45,8 @@ module.exports = `
         </section>
 
         <p>tx、ty、sx、sy的默认值为0，tw、th、sw、sh的默认值和图片宽高相同。</p>
+
+        <p>这里的locate是元素的定位方式。如果值为lt，代表以tx和ty作为左上角（left-top）顶点，向右下方绘制tw和th的长度。<strong>默认值为center</strong>，代表以tx和ty为中心进行绘制。</p>
 
         <p class="tip">Tips：这八个参数（tx、ty、tw、th、sx、sy、sw、sh）也是HTML5的canvas提供的drawImage方法使用的参数。在mozilla的文档中，参数依次被称为“dx, dy, dWidth, dHeight, sx, sy, sWidth, sHeight”。其中d代表destination，对应Easycanvas的t，指target；s是source image的含义。</p>
 
